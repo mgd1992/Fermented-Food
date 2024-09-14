dedae0f7919a5d2c79119af4998c503c2382998b
marco garbini <marcoogarbini@gmail.com>
2024-09-14 14:51:49 +0200

    funcionalidades de logica basicas listas

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ApplicationRecord
   has_many :ferment_recipes
 
   validates :first_name, presence: true
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
+
 end
