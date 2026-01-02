6ca962f8e0fc7a296e11e26f5543cad5c6cdcbc0
marco garbini <marcoogarbini@gmail.com>
2026-01-02 09:35:14 +0100

    Configurando Claudinary

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "@rails/ujs"
-
 import Rails from "@rails/ujs"
 Rails.start()
+
+import "@hotwired/turbo-rails"
+import "./controllers"
+// import "@rails/ujs"
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+
