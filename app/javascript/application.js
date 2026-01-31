bd50bfeaa8d549d0c1f2adbcfd089e199071d196
marco garbini <marcoogarbini@gmail.com>
2026-01-31 20:27:56 +0100

    Fix: Add Bootstrap JS bundle for mobile menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
-import Rails from "@rails/ujs"
-Rails.start()
+import Rails from "@rails/ujs";
+Rails.start();
 
-import "@hotwired/turbo-rails"
-import "./controllers"
+import "@hotwired/turbo-rails";
+import "./controllers";
+import "bootstrap";
 // import "@rails/ujs"
