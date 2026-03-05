40400633e9a3064b4a080ccea20895bee2a17226
marco garbini <marcoogarbini@gmail.com>
2026-03-05 16:55:37 +0100

    Agregar notificaciones para comentarios en fermentos y mensajeria con Turbo Streams alineado con rubocop

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,7 +6,7 @@ class Notification < ApplicationRecord
   scope :unread, -> { where(read: false) }
   scope :recent, -> { order(created_at: :desc).limit(10) }
 
-  after_create_commit -> {
+  after_create_commit lambda {
     broadcast_prepend_to(
       "notifications_#{user_id}",
       target: "notifications_list",
